88cc9e53e92954dd433b5b4801b1b4ada1756bd2
Андреев Антон <obsos32@gmail.com>
2023-04-19 09:45:28 +0300

    Тест-коммит

---
+++ first_script.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+import os
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+print(os.path.abspath(__file__))
+print(os.path.abspath(os.path.dirname(__file__)))
