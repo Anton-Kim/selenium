481f5bd5504cfc9daf3e6baec86f5557d54c5596
Андреев Антон <obsos32@gmail.com>
2023-04-20 09:57:49 +0300

    structure reformat

--- 2.4_step8.py
+++ 2.4_step8.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
 
 def calc(n):
   return str(math.log(abs(12*math.sin(int(n)))))
--- first_script.py
+++ first_script.py
@@ -3,9 +3,35 @@ from selenium.webdriver.common.by import By
 import time
 
 
-import os
+# def test_substring(full_string, substring):
+#     assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-print(os.path.abspath(__file__))
-print(os.path.abspath(os.path.dirname(__file__)))
+link = 'http://suninjuly.github.io/registration2.html'
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your first name"]')
+    input1.send_keys('Ivan')
+    input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]')
+    input2.send_keys('Petrov')
+    input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]')
+    input3.send_keys('ivan@mail.ru')
+
+    button = browser.find_element(By.CSS_SELECTOR, 'button.btn')
+    button.click()
+
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    welcome_text = welcome_text_elt.text
+
+    assert "Congratulations! You have successfully registered!" == welcome_text
+finally:
+    time.sleep(10)
+    browser.quit()
