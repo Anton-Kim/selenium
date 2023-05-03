9d594ee77f360ee071679b6bbea001e8cde7abad
Андреев Антон <obsos32@gmail.com>
2023-05-03 22:59:03 +0300

    solution 3.6 step 10 (v.2)

---
+++ 3.6_step5.py
@@ -0,0 +1,35 @@
+import time
+import math
+import pytest
+from selenium.webdriver.common.by import By
+
+# 8 задач
+
+
+@pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1',
+                                  'https://stepik.org/lesson/236896/step/1',
+                                  'https://stepik.org/lesson/236897/step/1',
+                                  'https://stepik.org/lesson/236898/step/1',
+                                  'https://stepik.org/lesson/236899/step/1',
+                                  'https://stepik.org/lesson/236903/step/1',
+                                  'https://stepik.org/lesson/236904/step/1',
+                                  'https://stepik.org/lesson/236905/step/1'])
+class TestStepik:
+    def test_stepik_login(self, browser, link):
+        browser.get(link)
+
+        browser.find_element(By.ID, 'ember33').click()
+        browser.find_element(By.ID, 'id_login_email').send_keys('mail@mail.ru')
+        browser.find_element(By.ID, 'id_login_password').send_keys('pass')
+        browser.find_element(By.CSS_SELECTOR, '[class="sign-form__btn button_with-loader "]').click()
+
+        answer = math.log(int(time.time()))
+        print(answer)
+        time.sleep(2.5)
+        browser.find_element(By.CSS_SELECTOR, 'textarea').send_keys(str(answer))
+        time.sleep(0.5)
+        browser.find_element(By.CLASS_NAME, 'submit-submission').click()
+
+        message = browser.find_element(By.CLASS_NAME, 'smart-hints__hint').text
+        if message != 'Correct!':
+            print(message)
